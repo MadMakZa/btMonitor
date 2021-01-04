d72d30a68f22064bbc1689b0d4925d22cf794fff
MadMakZa <immakza@gmail.com>
2021-01-04 20:54:04 +0300

    intent method activate bluetooth

--- app/src/main/java/com/example/btmonitor/MainActivity.java
+++ app/src/main/java/com/example/btmonitor/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.btmonitor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private MenuItem menuItem;
     private BluetoothAdapter btAdapter;
+    private final int ENABLE_REQUEST = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
         setBtIcon();
         return super.onCreateOptionsMenu(menu);
     }
+    //слушатель нажатия кнопок в менюшке
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //если нажато, то включить блутуз
+        if(item.getItemId() == R.id.id_bt_button){
+            //если не включен
+            if(!btAdapter.isEnabled()){
+
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     //метод смены иконки блютуза
     private void setBtIcon(){
@@ -43,5 +59,12 @@ public class MainActivity extends AppCompatActivity {
         }else{
             menuItem.setIcon(R.drawable.ic_bt_enable); //показать кнопку для вкл
         }
+    }
+
+    //метод включения блутуза
+    private void enableBt(){
+        //запустить включение адаптера
+        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(intent, ENABLE_REQUEST);
     }
 }
