f0d94c85ff51a6ddb0eeb0afc5ab3cb2d3c4838a
MadMakZa <immakza@gmail.com>
2021-01-04 20:41:48 +0300

    add menu and icons bluetooth, create options menu

--- app/src/main/java/com/example/btmonitor/MainActivity.java
+++ app/src/main/java/com/example/btmonitor/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.btmonitor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //создание менюшки справа в углу
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        //найти кнопочку в меню и присвоить переменной
+        menuItem = menu.findItem(R.id.id_bt_button);
+        return super.onCreateOptionsMenu(menu);
     }
 }
