38ea0408351e03c53fd6c7bd10f34e7ab423da8b
MadMakZa <immakza@gmail.com>
2021-01-04 20:43:14 +0300

    add bluetooth adapter

--- app/src/main/java/com/example/btmonitor/MainActivity.java
+++ app/src/main/java/com/example/btmonitor/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.btmonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,11 +10,18 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private MenuItem menuItem;
+    private BluetoothAdapter btAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        //инициализация и доступ к блютуз адаптеру
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
     //создание менюшки справа в углу
