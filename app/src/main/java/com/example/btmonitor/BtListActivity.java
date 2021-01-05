7a530bf1d8fed87d4198e1c32138d8bf2cfae3dd
MadMakZa <immakza@gmail.com>
2021-01-05 22:17:57 +0300

    add new BtListActivity

---
+++ app/src/main/java/com/example/btmonitor/BtListActivity.java
@@ -0,0 +1,14 @@
+package com.example.btmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BtListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bt_list);
+    }
+}
