ba9a40179dd94c1ac2b58574b3009c8d3bc6d9c8
MadMakZa <immakza@gmail.com>
2021-01-05 22:57:10 +0300

    btAdapter rdy

--- app/src/main/java/com/example/btmonitor/adapter/BtAdapter.java
+++ app/src/main/java/com/example/btmonitor/adapter/BtAdapter.java
@@ -1,6 +1,7 @@
 package com.example.btmonitor.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.example.btmonitor.R;
 
 import java.util.List;
 
@@ -28,7 +31,21 @@ public class BtAdapter extends ArrayAdapter<ListItem> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         ViewHolder viewHolder;
 
-        if()
+        if(convertView == null){
+            viewHolder = new ViewHolder();
+            //надуть элемент вью
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.bt_list_item, null, false);
+            viewHolder.tvBtName = convertView.findViewById(R.id.tvBtName);
+            viewHolder.chBtSelected = convertView.findViewById(R.id.checkBox);
+            convertView.setTag(viewHolder);
+
+        } else {
+           viewHolder = (ViewHolder) convertView.getTag();
+
+        }
+
+        viewHolder.tvBtName.setText("text gadjets");
+        viewHolder.chBtSelected.setChecked(true);
 
         return super.getView(position, convertView, parent);
     }
