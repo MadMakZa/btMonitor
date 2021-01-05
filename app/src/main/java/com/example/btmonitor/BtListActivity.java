df77f50a34beac23c7f2aed8a37a0f7f8b291405
MadMakZa <immakza@gmail.com>
2021-01-05 22:33:11 +0300

    add logic for standart button home on action bar

--- app/src/main/java/com/example/btmonitor/BtListActivity.java
+++ app/src/main/java/com/example/btmonitor/BtListActivity.java
@@ -1,8 +1,11 @@
 package com.example.btmonitor;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class BtListActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class BtListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bt_list);
+        init();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //стандартная кнопка стрелочка назад на экшн баре сверху слева
+        if (item.getItemId() == android.R.id.home){
+            //закрыть активити
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void init(){
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
